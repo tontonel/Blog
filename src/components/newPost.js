abf0143f193d1bdb82bf397d05eef175dd3f189c
Ionut Tomutiu <tionut80@gmail.com>
2021-05-07 23:32:32 +0300

    adkljf

--- src/components/newPost.js
+++ src/components/newPost.js
@@ -25,7 +25,7 @@ export default function NewPost() {
                 title: title,
                 text: text
             }
-          }, { withCredentials: true });
+          };
     }
     return (
         <div>
