ad8e7e3714ec2db68b514de30145a0bc682e4a2e
Ionut Tomutiu <tionut80@gmail.com>
2021-05-09 22:41:45 +0300

    jakl

--- src/components/newPost.js
+++ src/components/newPost.js
@@ -37,7 +37,7 @@ export default function NewPost() {
             redirect: 'follow', // manual, *follow, error
             referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
             body: JSON.stringify(data) // body data type must match "Content-Type" header
-          });
+          }).then (res => console.log (res));
     }
     return (
         <div>
