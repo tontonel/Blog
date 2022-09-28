2c1bf99353d2f6fd2420b67d72edb906a3c05f44
tontonel <66739208+tontonel@users.noreply.github.com>
2022-09-29 02:23:55 +0300

    Delete OptionItem.jsx

--- src/components/NavBarComponents/OptionItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function OptionItem(props) {
-    //still there is only one option in this menu I wouldn't pass it through props 
-    return (
-        <li>Logout</li>
-    )
-}
