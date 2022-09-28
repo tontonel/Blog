5ba11c6c13a0f3b1b0ad02cc17a2e1f175f79161
ionut tomutiu <tionut80@gmail.com>
2022-09-29 02:06:17 +0300

    added logout for users

---
+++ src/components/NavBarComponents/OptionItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function OptionItem(props) {
+    //still there is only one option in this menu I wouldn't pass it through props 
+    return (
+        <li>Logout</li>
+    )
+}
